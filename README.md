b7ae7d54508d7d24f796103109c08db8aa121d75
5hayanB <baig.sh@proton.me>
2024-12-13 11:33:55 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# PDP Tier 1-A1
+
+## Name: Shayan Hassan Baig
+
+## Roll No.: 20B-045-CS
+
+### Setup
+```sh
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install Faker
+```
+Download the [CSV files](https://drive.google.com/file/d/1czV8a6AufbZA6Ane7u2-WJ7JP7gB7Aa-/view?usp=drive_link)
---
+++ csv_gen.py
@@ -0,0 +1,51 @@
+import csv, random
+
+from faker import Faker
+
+STUDENT_COUNT = 1_000_000
+MONTHS = (
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+)
+fake = Faker()
+
+if __name__ == '__main__':
+    # Unique -> id
+    student_headers = ['id', 'first_name', 'last_name', 'address', 'grade']
+    with open('students.csv', 'w') as f:
+        writer = csv.DictWriter(f, student_headers)
+        writer.writeheader()
+        for i in range(STUDENT_COUNT):
+            name = fake.name().split()
+            firstname = name[0]
+            lastname = name[1]
+            writer.writerow({
+                'id': i,
+                'first_name': firstname,
+                'last_name': lastname,
+                'address': fake.address(),
+                'grade': random.randint(1, 10)
+            })
+    fees_headers = ['id', 'amount', 'day', 'month', 'year']
+    with open('fees.csv', 'w') as f:
+        writer = csv.DictWriter(f, fees_headers)
+        writer.writeheader()
+        for i in range(STUDENT_COUNT):
+            for j in range(len(MONTHS)):
+                writer.writerow({
+                    'id': i,
+                    'amount': random.randint(5_000, 10_000),
+                    'day': random.randint(1, 30),
+                    'month': MONTHS[j],
+                    'year': 'Fall 2024'
+                })
